9ee54483f7695ae051bd5e8820c0224649c70846
Sheriff Pascal <nwodosheriff.p@gmail.com>
2025-02-12 20:58:14 +0100

    Finished Author Routes

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,14 +1,11 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginFileEncode,
-    FilePondPluginImageResize
-);
+FilePond.registerPlugin(FilePondPluginImagePreview);
+FilePond.registerPlugin(FilePondPluginFileEncode);
+FilePond.registerPlugin(FilePondPluginImageResize);
 
 FilePond.setOptions({
     stylePanelAspectRatio: 150 / 100,
     imageResizeTargetWidth: 100,
     imageResizeTargetHeight: 150
-
 });
 
 FilePond.parse(document.body);
